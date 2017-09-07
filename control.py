b44b0526dbdeb878d1df67b04b0db912d5d347c5
David Mishchenko <DavidM.BIZ707@gmail.com>
2017-09-06 22:01:37 -0700

    Update...

--- control.py
+++ control.py
@@ -2,25 +2,31 @@ import sys
 import drivers
 
 def run(request):
-        try:
-                if (len(request.form['run_mode']) > 0):
-                        run_mode = request.form['run_mode']
-                        drivers.run_mode()
-                else:
-                        run_mode = 'null'
-        except:
-                run_mode = 'null'
-
-        try:
-                turn = int(request.form['turn'])
-                drivers.turn(turn)
-        except:
-                turn = 'null'
-
-        try:
-                motor = int(request.form['motor'])
-                drivers.motor(motor)
-        except:
-                motor = 'null'
-
-        return 'null'
+	try:
+		if (len(request.form['run_mode']) > 0):
+			run_mode = request.form['run_mode']
+			drivers.run_mode()
+		else:
+			run_mode = 'null'
+	except:
+		run_mode = 'null'
+	
+	try: 
+		turn = int(request.form['turn']) 
+		drivers.turn(turn)
+	except: 
+		turn = 'null'
+	
+	try:
+		motor = int(request.form['motor'])
+		drivers.motor(motor)
+	except:
+		motor = 'null'
+	
+	try:
+		lights = int(request.form['lights'])
+		drivers.lights(lights)
+	except:
+		lights = 'null'
+	
+	return 'null'
--- drivers.py
+++ drivers.py
@@ -4,42 +4,93 @@ import RPi.GPIO as GPIO
 
 MotorA_1 = 17
 MotorA_2 = 27
+MotorA_PWM = 18
 MotorB_1 = 22
 MotorB_2 = 23
+MotorB_PWM = 13
+LED_WhiteL = 24
+LED_WhiteR = 25
+LED_RedL = 26
+LED_RedR = 16
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(MotorA_1, GPIO.OUT)
-GPIO.setup(MotorA_2, GPIO.OUT)
-GPIO.setup(MotorB_1, GPIO.OUT)
-GPIO.setup(MotorB_2, GPIO.OUT)
+def setup():
+	GPIO.setwarnings(False)
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(MotorA_1, GPIO.OUT)
+	GPIO.setup(MotorA_2, GPIO.OUT)
+	GPIO.setup(MotorB_1, GPIO.OUT)
+	GPIO.setup(MotorB_2, GPIO.OUT)
+	GPIO.setup(LED_WhiteL, GPIO.OUT)
+	GPIO.setup(LED_WhiteR, GPIO.OUT)
+	GPIO.setup(LED_RedL, GPIO.OUT)
+	GPIO.setup(LED_RedR, GPIO.OUT)
+	GPIO.setup(MotorA_PWM, GPIO.OUT)
+	GPIO.output(MotorA_PWM, GPIO.HIGH)
+	GPIO.setup(MotorB_PWM, GPIO.OUT)
+	GPIO.output(MotorB_PWM, GPIO.HIGH)
+	motorPWM = GPIO.PWM(MotorA_PWM,30)
 
-
-def run_mode():
-        motor(0)
-        turn(0)
+def clamp(n,n_min,n_max): return max(n_min, min(n,n_max))
 
 def motor(speed):
-        if speed > 0:
-                GPIO.output(MotorA_1, GPIO.LOW)
-                GPIO.output(MotorA_2, GPIO.HIGH)
-        elif speed < 0:
-                GPIO.output(MotorA_1, GPIO.HIGH)
+	speed = clamp(speed,-100,100)
+	if speed > 0:
+		motorPWM.changeDutyCycle(speed)
+		GPIO.output(MotorA_1, GPIO.LOW)
+		GPIO.output(MotorA_2, GPIO.HIGH)
+	elif speed < 0:
+		speed = -speed
+		motorPWM.changeDutyCycle(speed)
+		GPIO.output(MotorA_1, GPIO.HIGH)
+		GPIO.output(MotorA_2, GPIO.LOW)
+	else:
+		motorPWM.stop()
+		GPIO.output(MotorA_1, GPIO.LOW)
                 GPIO.output(MotorA_2, GPIO.LOW)
-        else:
-                GPIO.output(MotorA_1, GPIO.LOW)
-                GPIO.output(MotorA_2, GPIO.LOW)
-        return 'null'
 
 def turn(angle):
-        if angle > 0:
-                GPIO.output(MotorB_1, GPIO.LOW)
-                GPIO.output(MotorB_2, GPIO.HIGH)
-        elif angle < 0:
-                GPIO.output(MotorB_1, GPIO.HIGH)
-                GPIO.output(MotorB_2, GPIO.LOW)
-        else:
-                GPIO.output(MotorB_1, GPIO.LOW)
-                GPIO.output(MotorB_2, GPIO.LOW)
-        return 'null'
+	if angle > 0:
+		GPIO.output(MotorB_1, GPIO.HIGH)
+		GPIO.output(MotorB_2, GPIO.LOW)
+	elif angle < 0:
+		GPIO.output(MotorB_1, GPIO.LOW)
+		GPIO.output(MotorB_2, GPIO.HIGH)
+	else:
+		GPIO.output(MotorB_1, GPIO.LOW)
+		GPIO.output(MotorB_2, GPIO.LOW)
 
+def ledFL(state):
+	if state > 0:
+		GPIO.output(LED_WhiteL, GPIO.HIGH)
+	else:
+		GPIO.output(LED_WhiteL, GPIO.LOW)
+
+def ledFR(state):
+        if state > 0:
+                GPIO.output(LED_WhiteR, GPIO.HIGH)
+        else:
+                GPIO.output(LED_WhiteR, GPIO.LOW)
+
+def ledRL(state):
+        if state > 0:
+                GPIO.output(LED_RedL, GPIO.HIGH)
+        else:
+                GPIO.output(LED_RedL, GPIO.LOW)
+
+def ledRR(state):
+        if state > 0:
+                GPIO.output(LED_RedR, GPIO.HIGH)
+        else:
+                GPIO.output(LED_RedR, GPIO.LOW)
+
+def lights(state):
+	if state == 0:
+		ledFL(0)
+		ledFR(0)
+		ledRL(0)
+		ledRR(0)
+	else:
+		ledFL(1)
+		ledFR(1)
+		ledRL(1)
+		ledRR(1)
