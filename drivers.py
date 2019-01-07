02fa3dbe974a75e09b28b6a451a8641894c964a8
root <DavidM.BIZ707@gmail.com>
2019-01-07 06:13:43 +0000

    Revert backend to Button UI

--- drivers.py
+++ drivers.py
@@ -13,29 +13,36 @@ LED_WhiteR = 25
 LED_RedL = 26
 LED_RedR = 16
 
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(MotorA_1, GPIO.OUT)
+GPIO.setup(MotorA_2, GPIO.OUT)
+GPIO.setup(MotorB_1, GPIO.OUT)
+GPIO.setup(MotorB_2, GPIO.OUT)
+GPIO.setup(LED_WhiteL, GPIO.OUT)
+GPIO.setup(LED_WhiteR, GPIO.OUT)
+GPIO.setup(LED_RedL, GPIO.OUT)
+GPIO.setup(LED_RedR, GPIO.OUT)
+GPIO.setup(MotorA_PWM, GPIO.OUT)
+GPIO.output(MotorA_PWM, GPIO.HIGH)
+GPIO.setup(MotorB_PWM, GPIO.OUT)
+GPIO.output(MotorB_PWM, GPIO.HIGH)
+
+#motorPWM = GPIO.PWM(MotorA_PWM,30)
+#motorPWM.start(0)
+#turnPWM = GPIO.PWM(MotorB_PWM,30)
+#turnPWM.start(0)
+
+
 def setup():
-	GPIO.setwarnings(False)
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(MotorA_1, GPIO.OUT)
-	GPIO.setup(MotorA_2, GPIO.OUT)
-	GPIO.setup(MotorB_1, GPIO.OUT)
-	GPIO.setup(MotorB_2, GPIO.OUT)
-	GPIO.setup(LED_WhiteL, GPIO.OUT)
-	GPIO.setup(LED_WhiteR, GPIO.OUT)
-	GPIO.setup(LED_RedL, GPIO.OUT)
-	GPIO.setup(LED_RedR, GPIO.OUT)
-	GPIO.setup(MotorA_PWM, GPIO.OUT)
-	GPIO.output(MotorA_PWM, GPIO.HIGH)
-	GPIO.setup(MotorB_PWM, GPIO.OUT)
-	GPIO.output(MotorB_PWM, GPIO.HIGH)
-	#motorPWM = GPIO.PWM(MotorA_PWM,30)
-	#turnPWM = GPIO.PWM(MotorB_PWM,30)
+	#Empty for now, must resolve PWM global variable problem
+	#PWM must be in a variable, but when executed here, the variable is lost
+	return 'null'
 
 def clamp(n,n_min,n_max): return max(n_min, min(n,n_max))
 
 def motor(speed):
 	speed = clamp(speed,-100,100)
-	motorPWM = GPIO.PWM(MotorA_PWM,30)
 	if speed > 0:
 		GPIO.output(MotorA_1, GPIO.LOW)
 		GPIO.output(MotorA_2, GPIO.HIGH)
@@ -46,11 +53,10 @@ def motor(speed):
 	else:
 		GPIO.output(MotorA_1, GPIO.LOW)
 		GPIO.output(MotorA_2, GPIO.LOW)
-	motorPWM.ChangeDutyCycle(speed)
+	#motorPWM.ChangeDutyCycle(speed)
 
 def turn(angle):
 	angle = clamp(angle,-100,100)
-	turnPWM = GPIO.PWM(MotorB_PWM,30)
 	if angle > 0:
 		GPIO.output(MotorB_1, GPIO.HIGH)
 		GPIO.output(MotorB_2, GPIO.LOW)
@@ -61,7 +67,7 @@ def turn(angle):
 	else:
 		GPIO.output(MotorB_1, GPIO.LOW)
 		GPIO.output(MotorB_2, GPIO.LOW)
-	turnPWM.ChangeDutyCycle(angle)
+	#turnPWM.ChangeDutyCycle(angle)
 
 def ledFL(state):
 	if state > 0:
