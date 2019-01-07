ce83e730f64f4ff874e5059d0c18365ed66e04cb
root <DavidM.BIZ707@gmail.com>
2019-01-07 06:58:27 +0000

    Button UI

--- control.py
+++ control.py
@@ -5,35 +5,15 @@ def run(request):
 	turn = 'null'
 	motor = 'null'
 
-	#try:
-	#	if (len(request.form['run_mode']) > 0):
-	#		run_mode = request.form['run_mode']
-	#		drivers.run_mode()
-	#	else:
-	#		run_mode = 'null'
-	#except:
-	#	run_mode = 'null'
+	try:
+		turn = request.form['turn']
+		drivers.turn(int(turn))
+	except:
+		turn = 'null'
+	try:
+		motor = request.form['motor']
+		drivers.motor(int(motor))
+	except:
+		motor = 'null'
 
-	#try:
-	#	turn = request.form['turn']
-	#	drivers.turn(int(turn))
-	#except:
-	#	turn = 'null'
-
-	#try:
-	#	motor = request.form['motor']
-	#	drivers.motor(int(motor))
-	#except:
-	#	motor = 'null'
-
-	#try:
-	#	lights = int(request.form['lights'])
-	#	drivers.lights(lights)
-	#except:
-	#	lights = 'null'
-
-	drivers.turn(int(request.form['turn']))
-	drivers.motor(int(request.form['motor']))
-
-	text = turn + ' ' + motor
-	return text
+	return turn + ' ' + motor
